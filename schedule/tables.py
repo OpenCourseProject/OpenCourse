2f4f6b428f6aa71e939ad4d0056121725f9d655d
gravitylow <ad@mfendley.com>
2015-07-23 01:08:31 +0000

    Fix schedule ordering by time

--- schedule/tables.py
+++ schedule/tables.py
@@ -2,9 +2,9 @@ import django_tables2 as tables
 from course.models import Course
 
 class ScheduleTable(tables.Table):
-    days = tables.Column(accessor='primary_meeting_time.days', order_by='meeting_times.days')
-    start_time = tables.Column(accessor='primary_meeting_time.start_time', order_by='meeting_times.start_time')
-    end_time = tables.Column(accessor='primary_meeting_time.end_time', order_by='meeting_times.end_time')
+    days = tables.Column(accessor='primary_meeting_time.days', order_by='primary_meeting_time.days')
+    start_time = tables.Column(accessor='primary_meeting_time.start_time', order_by='primary_meeting_time.start_time')
+    end_time = tables.Column(accessor='primary_meeting_time.end_time', order_by='primary_meeting_time.end_time')
 
     class Meta:
         model = Course
@@ -15,9 +15,9 @@ class ScheduleTable(tables.Table):
         fields = ("crn", "course", "section", "title", "hours", "llc", "ctype", "days", "start_time", "end_time", "location", "instructor", "seats")
 
 class SchedulePrintTable(tables.Table):
-    days = tables.Column(accessor='primary_meeting_time.days', order_by='meeting_times.days')
-    start_time = tables.Column(accessor='primary_meeting_time.start_time', order_by='meeting_times.start_time')
-    end_time = tables.Column(accessor='primary_meeting_time.end_time', order_by='meeting_times.end_time')
+    days = tables.Column(accessor='primary_meeting_time.days', order_by='primary_meeting_time.days')
+    start_time = tables.Column(accessor='primary_meeting_time.start_time', order_by='primary_meeting_time.start_time')
+    end_time = tables.Column(accessor='primary_meeting_time.end_time', order_by='primary_meeting_time.end_time')
 
     class Meta:
         model = Course
